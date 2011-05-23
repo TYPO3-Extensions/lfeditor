d3aed5a6c98e8c788d2648ad08123f991522d6f3
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-23 14:39:56 +0000

    [TASK] Increase minimum php and TYPO3 requirements
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/lfeditor/trunk@48024 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -46,8 +46,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.0.0-5.2.99',
-			'typo3' => '3.8.0-4.2.99',
+			'php' => '5.2.0-5.3.99',
+			'typo3' => '4.3.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
