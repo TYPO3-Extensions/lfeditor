15c92feda3ddf5339ec5aca008cbba9681bc0ecc
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-19 14:44:26 +0000

    [FEATURE] Adjusted code for tinymce 4.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/lfeditor/trunk@76633 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod1/tinyMCEConfig.js
+++ mod1/tinyMCEConfig.js
@@ -1,30 +1,5 @@
-function myCustomCleanup(type, value) {
-	switch (type) {
-		case 'insert_to_editor':
-			value = value.replace("\n", '<br />');
-			break;
-	}
-	return value;
-}
-
-tinyMCE.init({
-	height: '100',
-
-	mode: 'textareas',
+tinymce.init({
+	selector: 'textarea',
 	entity_encoding: 'raw',
-	forced_root_block: false,
-	convert_newlines_to_brs: true,
-	cleanup_callback: 'myCustomCleanup',
-
-	theme: 'advanced',
-	theme_advanced_buttons1: 'bold,italic,underline,strikethrough,separator,forecolor,backcolor,charmap,separator,bullist,numlist,separator,code,fullscreen,spellchecker',
-	theme_advanced_buttons2: '',
-	theme_advanced_buttons3: '',
-	theme_advanced_toolbar_location: 'top',
-	theme_advanced_toolbar_align: 'left',
-	theme_advanced_path: false,
-	theme_advanced_statusbar_location: 'bottom',
-	theme_advanced_resizing: true,
-	theme_advanced_resize_horizontal: false,
-	plugins: 'fullscreen,spellchecker'
+	forced_root_block: false
 });
